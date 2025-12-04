534e9a785d5fa9f8fd7f4be83a54c7ffd8290bf2
Sarthak Manda <geetsarthakmanda@gmail.com>
2025-12-04 19:49:12 +0530

    basics of JavaScript

--- 1.js
+++ 1.js
@@ -1,1 +1,13 @@
-//test
+//basics/ variables 
+
+var Name = "Batman";
+let age = 35;
+const city = "Gotham";
+let game;
+//the game is returned as undefined because it is declared but not initialized
+
+console.log("Name:", Name);
+console.log("Age:", age);
+console.log("City:", city);
+
+console.table({Name, age, city, game});
